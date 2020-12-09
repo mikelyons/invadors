70a70620ceeda00b511a942ba22f68d072888546
Mike Lyons <lyons.mr@gmail.com>
2020-12-09 04:34:50 -0800

    some assets cleanup

--- assets/maps/earthmap/earthMap3.tsx
+++ assets/maps/earthmap/earthMap3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="earthMap3" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="assets/images/tilesheet.png" width="512" height="512"/>
+<tileset version="1.4" tiledversion="1.4.3" name="earthMap3" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../images/tilesheet.png" width="512" height="512"/>
 </tileset>
